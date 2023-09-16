791b7ddbdf8e607b84bfa7fba4f536e9cb37430f
umut7 <umut7@DESKTOP-3U4DR6O>
2023-09-16 19:12:09 +0200

    Add project files.

---
+++ Try and Cacth exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Try_and_Cacth_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 0;
+
+
+            // If the code in "try" works everything is fine but if did not console executes codes in cacth if exception in paranthesis is encountered
+            try
+            {
+                int result = num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Can't divide by zero");
+                Console.Read();
+            }
+
+
+
+        }
+    }
+}
